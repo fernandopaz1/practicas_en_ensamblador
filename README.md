72dfb0f1d3d3cacdb97ff5a22c6ad1e30a9fa90e
Fernando Paz <ferpaz9292@gmail.com>
2019-11-05 22:52:28 -0300

    Primer commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Esta son prácticas de ensamblador de la matería Organización del Computador de la Universidad General Sarmiento.
+
+Trabajamos con la arquitectura Intel 8086.
---
+++ ej1.asm
@@ -0,0 +1,37 @@
+org 100h
+
+mov ax, 12h
+mov bx, 34h
+mov cx, ffh
+mov dx, 00h
+
+;;;;;;
+;   Queremos intercambiar ax y bx sin usar los otros regisros
+;   Para eso podemos usar las funciones push y pop
+;   push agrega el elemento a la pila
+;   pop retama el ultimo valor que hay en la pila
+;;;;;;
+
+push ax
+mov ax, bx
+pop bx
+
+;;;;;;;;;;;;;
+; Si queremos que el valor numérico de cx sea el mismo que al
+;;;;;;;;;;;;;
+
+;limpiamos cx
+
+sub cx, cx
+mov cl,al
+
+
+;;;;;;;;;;;;;
+; Hacemos lo mismo con dx y ah
+;;;;;;;;;;;;;
+
+sub dx, dx
+mov dl, ah
+
+;Lo copiamos en la parte baja (low) del registro
+
